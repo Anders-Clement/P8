faa2457c5c443dc59d89d74b5dc9e2e318268212
HansLarsen <hlarse16@student.aau.dk>
2022-03-22 00:14:18 +0100

    Box script updates and presentation fixes

--- src/rob8/scripts/box_publisher.py
+++ src/rob8/scripts/box_publisher.py
@@ -9,33 +9,33 @@ def talker():
     pub = rospy.Publisher('/boxes', rob8.msg.Boxes, queue_size=10)
     rospy.init_node('talker', anonymous=True)
    
-    new_box = rob8.msg.Boxes()
-    new_box.boxid = 0
-    new_box.pose.position.x = 0.5
-    new_box.pose.position.y = 0.5
-    new_box.pose.position.z = 0.1
-    new_box.scalex = 0.2
-    new_box.scaley = 0.2
-    new_box.scalez = 0.1
-    new_box.pose.orientation.w = 1
-    new_box.type.data = "pickup"
-    rospy.loginfo(new_box)
-    pub.publish(new_box)
+    # new_box = rob8.msg.Boxes()
+    # new_box.boxid = 0
+    # new_box.pose.position.x = 0.5
+    # new_box.pose.position.y = 0.5
+    # new_box.pose.position.z = 0.1
+    # new_box.scalex = 0.2
+    # new_box.scaley = 0.2
+    # new_box.scalez = 0.1
+    # new_box.pose.orientation.w = 1
+    # new_box.type.data = "pickup"
+    # rospy.loginfo(new_box)
+    # pub.publish(new_box)
 
-    new_box = rob8.msg.Boxes()
-    new_box.boxid = 1
-    new_box.pose.position.x = -0.5
-    new_box.pose.position.y = -0.5
-    new_box.pose.position.z = 0.1
-    new_box.scalex = 0.2
-    new_box.scaley = 0.2
-    new_box.scalez = 0.1
-    new_box.pose.orientation.w = 1
-    new_box.type.data = "place"
-    rospy.loginfo(new_box)
-    pub.publish(new_box)
+    # new_box = rob8.msg.Boxes()
+    # new_box.boxid = 1
+    # new_box.pose.position.x = -0.5
+    # new_box.pose.position.y = -0.5
+    # new_box.pose.position.z = 0.1
+    # new_box.scalex = 0.2
+    # new_box.scaley = 0.2
+    # new_box.scalez = 0.1
+    # new_box.pose.orientation.w = 1
+    # new_box.type.data = "place"
+    # rospy.loginfo(new_box)
+    # pub.publish(new_box)
 
-    time.sleep(10)
+    # time.sleep(10)
 
     client = actionlib.SimpleActionClient('planner_executer', rob8.msg.ExecutepathAction)
     client.wait_for_server()
